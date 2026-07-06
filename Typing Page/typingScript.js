43f2826d5d06d46f9577f598c74f682564fffd2e
404NotFound <edrummer8247@gmail.com>
2026-07-06 16:34:48 +0100

    Improved Functionality on the typing test in the Javascript

--- Typing Page/typingScript.js
+++ Typing Page/typingScript.js
@@ -1,6 +1,7 @@
 let quoteText = document.getElementById("textBox")
 let characterType = document.getElementById("inputedText")
 const wpmElement = document.getElementById("typingSpeed")
+const accuracyText = document.getElementById("accuracy")
 const timerText = document.getElementById("timerText")
 
 const quotes = ["Hey, this is just a test", "Hello World.", "The quick brown fox jumps over the lazy dog."]
@@ -11,11 +12,13 @@ let startTime = 0
 let runningTime = 0
 let minutes = 0
 let seconds = 0
+let accuracy = 0
 let typedText = null
 
 function generateQuote() {
     quote = quotes[Math.floor(Math.random() * quotes.length)]
     quoteText.textContent = quote
+    startTyping()
 }
 
 function startTyping() {
@@ -26,7 +29,7 @@ function startTyping() {
 
 function updateTime() {
     typedText = characterType.value
-    if (running && (typedText.length <= quote.length)) {
+    if (running && (typedText.length < quote.length)) {
         runningTime = performance.now() - startTime
 
         seconds = Math.floor(runningTime / 1000) % 60
@@ -34,10 +37,12 @@ function updateTime() {
 
         
     timerText.textContent = minutes.toString() + ":" + seconds.toString().padStart(2, "0")
+    calculateResult()
     }
 
     else {
-        calculateResult()
+        characterType.disabled = true
+        generateResetButton()
     }
 }
 
@@ -50,22 +55,34 @@ function resetTime() {
 }
 
 function calculateResult() {
-    running = false
     let target = quote
     typedText = characterType.value
     let mistakes = 0
 
-    for (let i = 0; i < typedText.length; i++) {
+    for (let i = 0; i <= typedText.length; i++) {
         if (typedText[i] !== target[i]) {
             mistakes++
         }
     }
 
     let speed = Math.round(((target.length / 5) / (runningTime / 1000 / 60)))
-    let accuracy = typedText.length > 0 ? Math.round(((typedText.length - mistakes) / typedText.length) * 100) : null
+    accuracy = typedText.length > 0 ? Math.round(((typedText.length - mistakes) / typedText.length) * 100) : null
+    let totalSpeed = Math.round(speed * (accuracy / 100))
 
-    wpmElement.textContent = speed + " " + "WPM"
+    wpmElement.textContent = Math.max(totalSpeed, 0) + " " + "WPM"
+    accuracyText.textContent = Math.max(accuracy, 0) + "%"
 }
 
-setInterval(updateTime, 1000)
-document.addEventListener("DOMContentLoaded", () => {resetTime()})
+function generateResetButton() {
+    let resetButton = document.createElement("button")
+    resetButton.id = "resetButton"
+    resetButton.innerText = "Reset"
+
+    resetButton.onclick = () => {
+        resetTime()
+        runningTime = 0
+    }
+}
+
+setInterval(updateTime, 100)
+document.addEventListener("DOMContentLoaded", () => {generateQuote()})
